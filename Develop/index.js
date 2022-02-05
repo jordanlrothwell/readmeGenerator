0a14d21d02dade2a093584ec156fe92256a224c8
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-02-05 11:48:45 +1000

    Included inquirer + fs packages; added placeholder questions to array.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "list",
+    name: "license",
+    message: "",
+    choices: [],
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "",
+  },
+  {
+    type: "",
+    name: "features",
+    message: "",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
